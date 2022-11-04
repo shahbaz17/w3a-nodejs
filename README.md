024fc493a5a6d17cd02a0e41408fc6a3db65a8db
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-11-04 10:04:43 +0530

    node js example

---
+++ README.md
@@ -0,0 +1,12 @@
+# Web3Auth's Node SDK Example
+
+## Quickstart
+
+```bash
+git clone
+cd w3a-nodejs-example
+npm install
+npm start
+```
+
+Check https://web3auth.io/docs/sdk/web-backend for more info.
---
+++ index.js
@@ -0,0 +1,44 @@
+const { Web3Auth } = require('@web3auth/node-sdk');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const getToken = () => {
+	const privateKey = fs.readFileSync('privateKey.pem');
+	const token = jwt.sign(
+		{
+			sub: 'shahbaz@web3auth.io',
+			name: 'Mohammad Shahbaz Alam',
+			email: 'shahbaz@web3auth.io',
+			aud: 'urn:my-resource-server', // -> to be used in Custom Authentication as JWT Field
+			iss: 'https://my-authz-server', // -> to be used in Custom Authentication as JWT Field
+			iat: Math.floor(Date.now() / 1000),
+			exp: Math.floor(Date.now() / 1000) + 60 * 60,
+		},
+		privateKey,
+		{ algorithm: 'RS256', keyid: '955104a37fa903ed80c57145ec9e83edb29b0c45' },
+	);
+	return token;
+};
+
+const web3auth = new Web3Auth({
+	clientId:
+		'BBP_6GOu3EJGGws9yd8wY_xFT0jZIWmiLMpqrEMx36jlM61K9XRnNLnnvEtGpF-RhXJDGMJjL-I-wTi13RcBBOo', // Get your Client ID from Web3Auth Dashboard
+	chainConfig: {
+		chainNamespace: 'eip155',
+		chainId: '0x1',
+		rpcTarget: 'https://rpc.ankr.com/eth',
+	},
+});
+
+web3auth.init({ network: 'testnet' });
+
+const connect = async () => {
+	const provider = await web3auth.connect({
+		verifier: 'web3auth-core-custom-jwt', // replace with your verifier name
+		verifierId: 'shahbaz@web3auth.io', // replace with your verifier id, setup while creating the verifier on Web3Auth's Dashboard
+		idToken: getToken(), // replace with your newly created unused JWT Token.
+	});
+	const eth_private_key = await provider.request({ method: 'eth_private_key' });
+	console.log('ETH Private Key', eth_private_key);
+};
+connect();
